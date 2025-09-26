12e21d55de5e1280645e48734435261ea6d4c992
Edu233223 <l221080149@iztapalapa.tecnm.mx>
2025-09-26 12:26:37 -0600

    Se renombraron todos los html como suvista lo presentaba. Se elimino la carpeta de EDUARDO para agregarlos solo al TEMPLATES. Se checaron que todos los htmls funcionaran correctamente. Se estan haciendo las conexiones entre html,css y botones

--- app.py
+++ app.py
@@ -2,9 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("eduardo/panel_de_admin.html")
+@app.route("/adminqr")
+def admin_qr():
+    return render_template("Admin_qr.html")
+
+@app.route("/codigoqr")
+def codigo_qr():
+    return render_template("codigo_qr.html")
+
+@app.route("/crearcuenta")
+def crear_cuenta():
+    return render_template("crear_cuenta.html")
+
+@app.route("/crearestudiante")
+def crear_estudiante():
+    return render_template("crear_estudiante.html")
+
+@app.route("/crearprofesor")
+def crear_profesor():
+    return render_template("crear_profesor.html")
+
+@app.route("/iniciodesesion")
+def inicio_de_sesion():
+    return render_template("inicio_de_sesion.html")
+
+@app.route("/mapaestacionamiento")
+def mapa_de_estacionamiento():
+    return render_template("mapa_de_estacionamiento.html")
+
+@app.route("/paneldeadmin")
+def panel_de_admin():
+    return render_template("panel_de_admin.html")
+
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
